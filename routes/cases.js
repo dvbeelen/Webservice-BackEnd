d1f8fba3171fdfa6bda0ee76cb551d88524effbc
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2019-11-28 23:12:58 +0100

    Added Get-One, Patch and Delete routes

--- routes/cases.js
+++ routes/cases.js
@@ -13,8 +13,8 @@ router.get('/', async (req, res) => {
 });
 
 //Get one case
-router.get('/:id', (req, res) => {
-    res.send(req.params.id);
+router.get('/:id', getCaseId, (req, res) => {
+    res.json(res.cases);
 });
 
 //Create new case
@@ -35,13 +35,53 @@ router.post('/', async (req, res) => {
 });
 
 //Update existing case
-router.patch('/', (req, res) => {
-
+router.patch('/:id', getCaseId, async (req, res) => {
+    if(req.body.projectName != null){
+        res.cases.projectName = req.body.projectName;
+    }
+    if(req.body.clientName != null){
+        res.cases.clientName = req.body.clientName;
+    }
+    if(req.body.summary != null){
+        res.cases.summary = req.body.summary;
+    }
+    if(req.body.description != null){
+        res.cases.description = req.body.description;
+    }
+    try {
+        const updatedCase = await res.cases.save();
+        res.json(updatedCase);
+    } catch(err) {
+        res.status(400).json({ message: err.message});
+    }
 });
 
 //Delete case
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', getCaseId, async (req, res) => {
+    try {
+        await res.cases.remove();
+        res.json('Case deleted.')
+    } catch(err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
+//Middleware - Get id.
+
+async function getCaseId(req, res, next){
+    let cases
+    try {
+        cases = await Case.findById(req.params.id);
+        if (cases == null){
+            return res.status(404).json({ message: 'No case found that matches the given id.'});
+        }
+    } catch(err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.cases = cases;
+    next();
+}
+
+//Return result
 module.exports = router;
