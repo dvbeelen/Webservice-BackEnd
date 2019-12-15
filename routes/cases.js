f6a1f790e8f18ba12a8a3d09d30441117926972c
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2019-12-15 19:39:42 +0100

    Fixed details-OPTIONS requests

--- routes/cases.js
+++ routes/cases.js
@@ -4,19 +4,28 @@ const Case = require('../models/portfolio_cases');
 const pagination = require('../pagination'); 
 
 //Give back OPTIONS
-router.use(function(req, res, next) {
+router.options("/", function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Allow', 'GET, POST, OPTIONS');
+    res.send(200);
+    if (req.headers.accept != 'application/json'||req.headers.accept != 'application/x-www-form-urlencoded') {
+          res.sendStatus(400);
+        }
+    else {
+        next()
+    }
+});
 
-
-    //Check if method = OPTIONS
-    if ('OPTIONS' === req.method) {
-      //respond with 200
-      res.sendStatus(200);
-    } 
-    else if (req.headers.accept != 'application/json') {
+//Give back OPTIONS for details-page
+router.options("/:id", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,DELETE,OPTIONS');
+    res.header('Allow', 'GET,PUT,DELETE,OPTIONS');
+    res.send(200);
+    if (req.headers.accept != 'application/json') {
           res.sendStatus(400);
         }
     else {
@@ -26,6 +35,9 @@ router.use(function(req, res, next) {
 
 //Get all cases
 router.get('/', async (req, res) => {
+    if (req.headers.accept != 'application/json') {
+        res.status(400).json({"message": "Data is only send in a JSON-format."});
+    }
 
     try {
         const cases = await Case.find().lean().exec();
@@ -58,6 +70,11 @@ router.get('/', async (req, res) => {
 //Get one case
 router.get('/:id', getCaseId, (req, res) => {
 
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, OPTIONS');
+    res.header('Allow', 'GET, PUT, DELETE, OPTIONS');
+    
     const item = {}
     item.item = res.cases
     item._links = {
@@ -68,11 +85,8 @@ router.get('/:id', getCaseId, (req, res) => {
             href: `http://145.24.222.215:8000/cases/`
         }
     }
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header('Allow', 'GET, OPTIONS');
     res.json(item);
+  
 });
 
 //Create new case
@@ -94,26 +108,35 @@ router.post('/', async (req, res) => {
 
 //Update existing case
 router.put('/:id', getCaseId, async (req, res) => {
+    //Check and send projectName if correct.
     if(req.body.projectName != null){
         res.cases.projectName = req.body.projectName;
-    } else {
-        req.body.projectName = res.cases.projectName
     }
+    if (req.body.projectName === "") {
+        res.send(400).json({"message": "Projectname is required."})
+    } 
+    //Check and send clientName if correct.
     if(req.body.clientName != null){
         res.cases.clientName = req.body.clientName;
-    } else {
-        req.body.clientName = res.cases.clientName;
     }
+    if(req.body.clientName === "") {
+        res.send(400).json({"message": "Clientname is required."})
+    }
+    //Check and send summary if correct.
     if(req.body.summary != null){
         res.cases.summary = req.body.summary;
-    } else {
-        req.body.summary = res.cases.summary;
     }
+    if(req.body.summary === "") {
+        res.send(400).json({"message": "Summary is required."})
+    }
+    //Check and send description if correct.
     if(req.body.description != null){
         res.cases.description = req.body.description;
-    } else {
-        req.body.description = res.cases.description
+    } 
+    if(req.body.description === "") {
+        res.send(400).json({"message": "Description is required."})
     }
+
     try {
         const updatedCase = await res.cases.save();
         res.status(200).json(updatedCase);
